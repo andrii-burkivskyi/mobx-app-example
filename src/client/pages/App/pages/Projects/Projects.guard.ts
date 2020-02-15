8069741814dbfed99295eb557e2177b1df4d7b06
Andrey Burkivskiy <andrey@andrey.local>
2020-02-15 03:41:39 +0200

    fix bug

--- src/client/pages/App/pages/Projects/Projects.guard.ts
+++ src/client/pages/App/pages/Projects/Projects.guard.ts
@@ -3,13 +3,11 @@ import { LocationService } from "../../../../core/services/Location.service";
 import { Guard } from "../../../../core/decorators/guard/guard.decorator";
 import { GuardBase } from "../../../../core/decorators/guard/Guard.base";
 import { Routes } from "../../../../core/routes";
-import { ProjectsModals } from "./Projects.modals";
 
 @Guard("ProjectsGuard")
 export class ProjectsGuard extends GuardBase {
     constructor(
         private location: LocationService,
-        private modals: ProjectsModals
     ) { super(); }
 
     route = {
